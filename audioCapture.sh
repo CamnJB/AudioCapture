5916a74c8b3a42a0d8538198c51333a064cc93cb
CamnJB <82467513+CamnJB@users.noreply.github.com>
2021-12-11 19:57:35 +0000

    Update audioCapture.sh

--- audioCapture.sh
+++ audioCapture.sh
@@ -1,11 +1,13 @@
-SERVICE='ffmpeg'
-WORD=$(xclip -o -sel c)
-
- 
-if ps ax | grep -v grep | grep $SERVICE > /dev/null
-then
-   pkill ffmpeg
-else
-    ffmpeg -f pulse -i alsa_output.pci-0000_2a_00.4.analog-stereo -acodec libmp3lame -ab 320k AudioRecordings/$WORD.mp3
-fi
-
+  # you will need ffmpeg and xclip installed for the script to work               
+                                                                                  
+  SERVICE='ffmpeg'                                                                
+  WORD=$(xclip -o -sel c) #recording will be named whatever is in the clipboard   
+  SAVETO="/path/to/file"  #path where recordings will be saved    
+                                                                                  
+  if ps ax | grep -v grep | grep $SERVICE > /dev/null                             
+  then                                                                            
+      pkill ffmpeg                                                                
+      #xclip $SAVETO/$WORD.mp3 #copies file to clipboard to be pasted into anki *NOT WORKING YET*
+  else                                                                            
+      ffmpeg -f pulse -i alsa_output.pci-0000_2a_00.4.analog-stereo -acodec libmp3lame -ab 320k $SAVETO/$WORD.mp3
+  fi 
